0128a71b2f91ac51db18ea117f0531610e9347a7
Persianboy98 <persianman2098@gmail.com>
2024-05-29 17:36:29 +0430

    0

--- db.py
+++ db.py
@@ -35,3 +35,30 @@ class Database:
         except mysql.connector.Error as err:
             messagebox.showerror("Error", f"Database error:{err}")
             return None
+
+    def update_student(self, student_id, person):
+        try:
+                query = """
+                    UPDATE students SET meli = %s,first_name= %s, last_name = %s, age = %s, email =%s
+                    WHERE meli = %s
+                """
+                person = (person.meli, person.first_name, person.last_name, person.age, person.email, student_id)
+                self.cursor.execute(query, person)
+                self.connection.commit()
+                messagebox.showinfo("Success ","student updated succsessfully!")
+        except mysql.connector.Error as err:
+                messagebox.showwarning("Error", f"Database error : {err}")
+
+    def delete_student(self, meli):
+
+        try:
+            query = "DELETE FROM students WHERE meli = %s"
+            self.cursor.execute(query, (meli,))
+            self.connection.commit()
+            if self.cursor.rowcount > 0:
+                messagebox.showinfo("Success","Student not found!")
+            else:
+                messagebox.showwarning("succes", "student updatet successfully!")
+
+        except mysql.connector.Error as err:
+            messagebox.showwarning("Error", f"Database error:{err}")
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter.messagebox as messagebox
 from db import Database
 from ttkbootstrap import Treeview
 
+
 class StudentManagementApp(tk.Tk):
 
     def __init__(self):
@@ -61,37 +62,114 @@ class StudentManagementApp(tk.Tk):
         btn_clear = tk.Button(self, text='Clear entries', command=self.clear_entries)
         btn_clear.grid(row=7, column=0, padx=10, pady=10)
 
-
     def edit_student(self):
-        pass
+        try:
+            if self.selected_student:
+                edit_window = tk.Toplevel(self)
+                edit_window.title("Edit Student")
+                student_data = self.selected_student
+
+                lbl_id = tk.Label(edit_window, text='Meli code')
+                lbl_id.grid(row=0, column=0, padx=10, pady=10)
+                self.entry_edit_id = tk.Entry(edit_window, state='normal')
+                self.entry_edit_id.grid(row=0, column=1, padx=10, pady=10)
+                self.entry_edit_id.insert(tk.END, student_data[0])
+
+                lbl_first_name = tk.Label(edit_window, text='First name:')
+                lbl_first_name.grid(row=1, column=0, padx=10, pady=10)
+                self.entry_edit_first_name = tk.Entry(edit_window)
+                self.entry_edit_first_name.grid(row=1, column=1, padx=10, pady=10)
+                self.entry_edit_first_name.insert(tk.END, student_data[1])
+
+                lbl_last_name = tk.Label(edit_window, text='Last_name')
+                lbl_last_name.grid(row=2, column=0, padx=10, pady=10)
+                self.entry_edit_last_name = tk.Entry(edit_window)
+                self.entry_edit_last_name.grid(row=2, column=1, padx=10, pady=10)
+                self.entry_edit_last_name.insert(tk.END, student_data[2])
+
+                lbl_age = tk.Label(edit_window, text='Age')
+                lbl_age.grid(row=3, column=0, padx=10, pady=10)
+                self.entry_edit_age = tk.Entry(edit_window)
+                self.entry_edit_age.grid(row=3, column=1, padx=10, pady=10)
+                self.entry_edit_age.insert(tk.END, student_data[3])
+
+                lbl_email = tk.Label(edit_window, text='Email')
+                lbl_email.grid(row=4, column=0, padx=10, pady=10)
+                self.entry_edit_email = tk.Entry(edit_window)
+                self.entry_edit_email.grid(row=4, column=1, padx=10, pady=10)
+                self.entry_edit_email.insert(tk.END, student_data[4])
+
+                btn_save = tk.Button(edit_window, text='save changes',
+                                     command=lambda: self.database.update_student(self.selected_student[0],
+                                                                                  Person(self.entry_edit_id.get(),
+                                                                                         self.entry_edit_first_name.get(),
+                                                                                         self.entry_edit_last_name.get(),
+                                                                                         self.entry_edit_age.get(),
+                                                                                         self.entry_edit_email.get())))
+                btn_save.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+
+
+
+
+
+
+
+
+
+
+
+            else:
+                messagebox.showwarning('warning', "please select a student to edit!")
+        except:
+            messagebox.showwarning('warning', "please select a student to edit!")
 
     def view_student(self):
 
         view_window = tk.Toplevel(self)
         view_window.title('view student')
 
-        title_label= tk.Label(view_window, text="All students", font=('Arial', 16))
+        title_label = tk.Label(view_window, text="All students", font=('Arial', 16))
         title_label.pack(pady=10)
 
         student_grid = Treeview(view_window, columns=("meli", "first_name", "last_name", "age", "email"),
                                 show="headings")
 
-        student_grid.heading("meli",text="meli code")
+        student_grid.heading("meli", text="meli code")
         student_grid.heading("first_name", text="First Name")
         student_grid.heading("last_name", text="Last Name")
         student_grid.heading("age", text="Age")
         student_grid.heading("email", text="Email")
         student_grid['show'] = 'headings'
 
-        student = self.database.get_all_student()
+        def on_select(event):
+            item_id = student_grid.selection()[0]
+            self.selected_student = student_grid.item(item_id, 'values')
 
-        for student in student:
+        student_grid.bind("<<TreeviewSelect>>", on_select)
+
+        students = self.database.get_all_student()
+
+        for student in students:
             student_grid.insert("", tk.END, values=student)
 
         student_grid.pack(fill=tk.BOTH, expand=True)
 
     def del_student(self):
-        pass
+        if self.selected_student:
+            try:
+
+                if messagebox.askyesno("confirm Deletion","Are you sure you want to delete this student?"):
+                    meli_code = self.selected_student[0]
+                    self.database.delete_student(meli_code)
+
+
+
+                    self.selected_student = None
+
+            except:
+                messagebox.showwarning("Error","An error occurred while deleting student!")
+        else:
+            messagebox.showwarning("warning","please select a student to delete")
 
     def clear_entries(self):
         self.entry_id.delete(0, tk.END)
